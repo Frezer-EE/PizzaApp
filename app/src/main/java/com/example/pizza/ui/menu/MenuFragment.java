7604aec3a448b56399fc294950bc4c69cdd944de
Ivan L <lopatin.ie@gmail.com>
2020-11-20 11:39:27 +0500

    Add Bottom Navigation Activity

---
+++ app/src/main/java/com/example/pizza/ui/menu/MenuFragment.java
@@ -0,0 +1,20 @@
+package com.example.pizza.ui.menu;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.pizza.R;
+
+public class MenuFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.activity_menu, container, false);
+        return root;
+    }
+}
