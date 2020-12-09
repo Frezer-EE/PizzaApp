7d0cfa961481b68d1472cf8a0157238f25c050f8
Ivan L <lopatin.ie@gmail.com>
2020-12-09 18:07:33 +0500

    Add animation gradient on splash screen activity

--- app/src/main/java/com/example/pizza/MainActivity.java
+++ app/src/main/java/com/example/pizza/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.pizza;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 
 public class MainActivity extends AppCompatActivity {
 
+    ConstraintLayout constraintLayoutMain;
+    AnimationDrawable animationDrawable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new CountDownTimer(3000, 1000) {
+        constraintLayoutMain = findViewById(R.id.splash_screen);
+        animationDrawable = (AnimationDrawable) constraintLayoutMain.getBackground();
+        animationDrawable.setEnterFadeDuration(0);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+        new CountDownTimer(8000, 1000) {
             @Override
             public void onTick(long l) {
             }
